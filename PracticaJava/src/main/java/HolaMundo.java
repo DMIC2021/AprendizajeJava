d09a0c30a95c7a5d43387b5ed1a31d508bef8243
Cristhian <cristhian.martinez-s@mail.escuelaing.edu.co>
2022-02-03 12:50:42 -0500

    Segunda práctica: Manejo de variables

--- PracticaJava/src/main/java/HolaMundo.java
+++ PracticaJava/src/main/java/HolaMundo.java
@@ -1,5 +1,10 @@
 public class HolaMundo {
     public static void main(String arg[]){
-        System.out.println("Hola Mundo");
+        String saludar = "Hola a todos, es un gusto";
+        System.out.println(saludar);
+        var despedirse = "Hasta una proxima oportunidad";
+        System.out.println(despedirse);
+        var telefono = "3100000000";
+        System.out.println(telefono);
     }
 }
