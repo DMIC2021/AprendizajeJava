25702d54f434b6690bb01e5c42609ee305b9d07d
Cristhian <cristhian.martinez-s@mail.escuelaing.edu.co>
2022-02-03 17:39:51 -0500

    Tercera práctica: Concatenación de strings y reglas paa definir variables

--- PracticaJava/src/main/java/HolaMundo.java
+++ PracticaJava/src/main/java/HolaMundo.java
@@ -1,10 +1,22 @@
 public class HolaMundo {
     public static void main(String arg[]){
+        /*HOLA MUNDO
         String saludar = "Hola a todos, es un gusto";
         System.out.println(saludar);
+        */
+        /*MANEJO DE VARIABLES
         var despedirse = "Hasta una proxima oportunidad";
         System.out.println(despedirse);
         var telefono = "3100000000";
         System.out.println(telefono);
+        */
+        //CONCATENACION
+        var usuario = "Juan";
+        var saludar = "Hola, como vas ";
+        var uno = 1;
+        var tres = 3;
+        System.out.println(saludar+usuario+uno+tres);
+        System.out.println(uno+tres+saludar+usuario);
+
     }
 }
